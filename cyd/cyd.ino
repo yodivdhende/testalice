f92b70a543590e4e94b0b6ace826ae6f005726f8
Yodi <yodi.vandenhende@gmail.com>
2025-03-17 23:36:33 +0100

    cyd: connecting to wifi

---
+++ cyd/cyd.ino
@@ -0,0 +1,41 @@
+#include <TFT_eSPI.h>
+#include <WiFi.h>
+
+TFT_eSPI tft = TFT_eSPI();
+
+void setup() {
+  screenSetup();
+  tft.println("booting");
+  connectToWifi();
+}
+
+void loop() {
+
+}
+
+void screenSetup() {
+  tft.init();
+  clearScreen();
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextFont(2);
+}
+
+void clearScreen() {
+  tft.fillScreen(TFT_BLACK);
+}
+
+
+void connectToWifi() {
+  char* ssid = "De Notenboom ";
+  char* password = "X4k9*9wS!K2eEp2b";
+  WiFi.begin(ssid, password);
+   tft.println((String)"connecting to: " + ssid);
+  while(WiFi.status() != WL_CONNECTED) {
+     tft.print(".");
+    delay(1000);
+  }
+  tft.print("\n");
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.println((String)"connected with Ip: "+ WiFi.localIP());
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+}
