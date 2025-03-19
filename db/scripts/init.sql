20eee1979a912420c5d27acf68d64dbf6e66525a
Yodi <yodi.vandenhende@gmail.com>
2025-03-19 23:03:50 +0100

    adding characters to db

--- db/scripts/init.sql
+++ db/scripts/init.sql
@@ -26,17 +26,38 @@ INSERT INTO Roles (Name)
 VALUES ('Admin'),
 ('Player'),
 ('Extra')
+;
 
 DROP TABLE IF EXISTS Users_Roles;
 CREATE TABLE Users_Roles (
   User int,
   Role int,
-  CONSTRAINT PK_Users_Roles PRIMARY KEY (User, Role)
-  FOREIN KEY (USER) REFERENSES USES(Id)
-  FOREIN KEY (Roles) REFERENSES Roles(Id)
+  CONSTRAINT PK_Users_Roles PRIMARY KEY (User, Role),
+  FOREIGN KEY (USER) REFERENCES Users(Id),
+  FOREIGN KEY (Role) REFERENCES Roles(Id)
 );
 
 INSERT INTO Users_Roles (User, Role)
 VALUES (1, 1),
 (2, 2),
 (3, 3)
+;
+
+DROP TABLE IF EXISTS Characters;
+CREATE TABLE Characters (
+  Id int NOT NULL AUTO_INCREMENT,
+  Name varchar(254),
+  Owner int,
+  CurrentHP int,
+  MaxHp int,
+  PRIMARY KEY (Id),
+  FOREIGN KEY (Owner) REFERENCES Users(Id)
+)
+;
+
+INSERT INTO Characters (Name, Owner, CurrentHP, MaxHP)
+VALUES ('Bob', 1, 100, 100)
+
+
+
+
