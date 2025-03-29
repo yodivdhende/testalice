d2b93b76e6228ffb18ca51c497fa074a1a7a7217
yodi.vandenhende <yodi.vandenhende@gmail.com>
2025-03-29 16:02:20 +0100

    Revert "init platform io"
    This reverts commit cbae8344dd44ca8bc687299bc67e8d43b5320313.

---
+++ cyd/character.ino
@@ -0,0 +1,52 @@
+// unsigned long lastTime = 0;
+// unsigned long timerDelay = 5000;
+
+void fetchCharacter() {
+  if(WiFi.status() == WL_CONNECTED) {
+    String characterUrl = server_url + "/characters?id=" + String(character_id);
+    logWhite("fetching: ");
+    logWhite(characterUrl.c_str());
+
+    String characterResponse = httpGETRequest(characterUrl.c_str());
+    Serial.println(characterResponse);
+
+    JsonDocument characterObj;
+    DeserializationError error = deserializeJson(characterObj, characterResponse);
+
+    if(error) {
+      logRed(error.c_str());  
+    }
+
+    String name = characterObj["name"];
+    logGreen(name.c_str());
+  }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, serverName);
+  
+  // If you need Node-RED/server authentication, insert user and password below
+  //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "{}"; 
+  
+  if (httpResponseCode>0) {
+    logWhite("HTTP Response code: %s", String(httpResponseCode));
+    payload = http.getString();
+  }
+  else {
+    logRed("Error code: ");
+    logRed(String(httpResponseCode).c_str());
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
--- cyd/src/gif.ino
+++
@@ -1,6 +0,0 @@
-
-void createGifFromFile(char *path) {
-  lv_obj_t *gif = lv_gif_create(lv_scr_act());
-  lv_gif_set_src(gif, path);
-  lv_obj_center(gif);
-}
