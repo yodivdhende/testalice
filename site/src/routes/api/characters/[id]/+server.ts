fa25c0774262256ba99decac86d4c276f934a869
Yodi <yodi.vandenhende@gmail.com>
2025-04-01 21:35:23 +0200

    implemeting dynamic rout for api

---
+++ site/src/routes/api/characters/[id]/+server.ts
@@ -0,0 +1,10 @@
+import { characterRepo } from "$lib/db/character.repo.svelte";
+import { type RequestHandler, json } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({params}) => {
+    const id = params.id;
+    if(id == null || typeof id != 'string') return json([]);
+    const idNumber = parseInt(id);
+    if(Number.isNaN(idNumber)) return json([]);
+    return json(await characterRepo.getById(idNumber));
+}
