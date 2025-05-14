1d776788c7f1cae06ceca7d46adc70b17eb848c9
Yodi <yodi.vandenhende@gmail.com>
2025-05-14 23:11:33 +0200

    creating websocket server

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,3 +1,7 @@
 <script lang="ts">
+    import {io } from 'socket.io-client';
+    const socket = io();
+     socket.on('eventFromServer', (message)=> console.log(`%c ️🛰️`, `background:aqua;color:black`, {message}));
+    
 </script>
 
--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,6 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { Server } from 'socket.io';
+import { defineConfig, type ViteDevServer } from 'vite';
+
+const webSocketServer = {
+	name: 'webSocketServer',
+	configureServer(server: ViteDevServer) {
+		if(!server.httpServer) return;
+		const io = new Server(server.httpServer);
+		io.on('connection', (socket)=>{
+			socket.emit('eventFromServer', 'Hello World 👋')
+		})
+	}
+}
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), webSocketServer]
 });
