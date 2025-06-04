10e1ad01a7f20fc2ce97de624df30f7f7eb77a28
yodi.vandenhende <yodi.vandenhende@gmail.com>
2025-06-04 17:06:20 +0200

    trying to fix touch screen

--- cyd/src/globals.h
+++ cyd/src/globals.h
@@ -1,15 +1,18 @@
 #ifndef GLOBALS
 #define GLOBALS
 
-    #include <TFT_eSPI.h>
-    #include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <Arduino.h>
 
-    extern TFT_eSPI tft;
-    extern String wifi_ssid;
-    extern String wifi_password;
-    extern String server_url;
-    extern int character_id;
-    void clearScreen();
-    void screenSetup();
+extern const uint16_t screenWidth;
+extern const uint16_t screenHeight;
+extern TFT_eSPI tft;
+extern String wifi_ssid;
+extern String wifi_password;
+extern String server_url;
+extern int character_id;
+extern XPT2046_Touchscreen ts;
+void clearScreen();
+void screenSetup();
 
 #endif
