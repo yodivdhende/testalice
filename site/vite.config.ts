f0fc4d693d288a496b8179047991da2b68d876e0
Yodi <yodi.vandenhende@gmail.com>
2025-12-15 15:26:24 +0100

    adding config foor railwayu

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,17 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, type ViteDevServer } from 'vite';
-import { WebSocketMidiator } from './websocket-server/socket-server';
-import * as http from "http";
+import { defineConfig } from 'vite';
 
-const webSocketServer = {
-	name: 'webSocketServer',
-	configureServer(server: ViteDevServer) {
-		if(!server.httpServer) return;
-		new WebSocketMidiator(server.httpServer as http.Server, server.config.server.port);
-	}
-}
+// const webSocketServer = {
+// 	name: 'webSocketServer',
+// 	configureServer(server: ViteDevServer) {
+// 		if(!server.httpServer) return;
+// 		new WebSocketMidiator(server.httpServer as http.Server, server.config.server.port);
+// 	}
+// }
 
 export default defineConfig({
-	plugins: [sveltekit(), webSocketServer],
+	plugins: [sveltekit()],
 	assetsInclude: ['**/*.glb'],
 });
